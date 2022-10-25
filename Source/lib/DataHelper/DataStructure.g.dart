7f1a284336c062c69d73d0afaee1c30fa323d178
Johannes Zill <Johannes.Zill@MITUTOYO-CTL.DE>
2022-10-25 15:31:15 +0200

    Added basic data structure for the questions and questionstacks

---
+++ Source/lib/DataHelper/DataStructure.g.dart
@@ -0,0 +1,40 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'DataStructure.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+QuestionStack _$QuestionStackFromJson(Map<String, dynamic> json) {
+  $checkKeys(
+    json,
+    requiredKeys: const ['name'],
+  );
+  return QuestionStack(
+    json['name'] as String,
+    (json['orderList'] as List<dynamic>).map((e) => e as String).toList(),
+    questionStringAndAnswers: (json['questionStringAndAnswers']
+                as List<dynamic>?)
+            ?.map((e) =>
+                QuestionStringAndAnswers.fromJson(e as Map<String, dynamic>))
+            .toList() ??
+        [],
+    questionStringAndFreeText: (json['questionStringAndFreeText']
+                as List<dynamic>?)
+            ?.map((e) =>
+                QuestionStringAndFreeText.fromJson(e as Map<String, dynamic>))
+            .toList() ??
+        [],
+  );
+}
+
+Map<String, dynamic> _$QuestionStackToJson(QuestionStack instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'questionStringAndAnswers':
+          instance.questionStringAndAnswers.map((e) => e.toJson()).toList(),
+      'questionStringAndFreeText':
+          instance.questionStringAndFreeText.map((e) => e.toJson()).toList(),
+      'orderList': instance.orderList,
+    };
