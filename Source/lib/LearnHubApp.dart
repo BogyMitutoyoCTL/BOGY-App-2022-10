3b07bef1f3b894f92aefe3a4687bbe4e355492c8
Kimi Roth <kimiroth280907@gmail.com>
2022-10-25 11:35:00 +0200

    Defauld Farbe geändert

--- Source/lib/LearnHubApp.dart
+++ Source/lib/LearnHubApp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learnhub/Score.dart';
 import 'WelcomePage.dart';
 
 class LearnHubApp extends StatelessWidget {
@@ -19,7 +20,7 @@ class LearnHubApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
       home: const WelcomePage(),
     );
