0a97de9418536f09048e602401dc1814189e5a13
Johannes Beier <gevatter_thanatos@protonmail.com>
2022-10-25 10:26:34 +0200

    Added image_picker

--- Source/lib/EditQuestion.dart
+++ Source/lib/EditQuestion.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class EditQuestion extends StatefulWidget {
   const EditQuestion({Key? key}) : super(key: key);
@@ -8,12 +9,52 @@ class EditQuestion extends StatefulWidget {
 }
 
 class _EditQuestionState extends State<EditQuestion> {
+  //false = Frage; true = Bild
+  bool questionType = false;
+  final TextEditingController _titleControll = TextEditingController();
+  String _title = "";
+  final ImagePicker _picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _titleControll.text = _title;
+    _titleControll.addListener(() {
+      setState(() {
+        _title = _titleControll.text;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Karte bearbeiten"),
       ),
+      body: Column(
+        children: [
+          RadioListTile(
+            value: false,
+            groupValue: questionType,
+            onChanged: questionTypeChange,
+            title: Text("Frage"),
+          ),
+          RadioListTile(
+            value: true,
+            groupValue: questionType,
+            onChanged: questionTypeChange,
+            title: Text("Bild"),
+          ),
+          Text("Frage:"),
+          TextField(
+            controller: _titleControll,
+          ),
+        ],
+      ),
     );
   }
 }
+
+void questionTypeChange(bool? type) {}
