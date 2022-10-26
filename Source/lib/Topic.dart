5f7fec5a8d61ecf788b9a3a11e7fb45f54d0a5ac
Johannes Beier <gevatter_thanatos@protonmail.com>
2022-10-26 11:43:52 +0200

    added QuestionStack to Topic

--- Source/lib/Topic.dart
+++ Source/lib/Topic.dart
@@ -3,20 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:learnhub/EditQuestion.dart';
 import 'package:learnhub/Score.dart';
 
+import 'DataHelper/QuestionStack.dart';
 import 'EditDeck.dart';
 import 'Question.dart';
 
 class Topic extends StatefulWidget {
   //false = Benutzereingabe; true= Multiple Choice
   //false = Home; true= EditDeck
-  bool answerType;
+  bool answerType = false;
   bool comingFrom;
-  String title;
+  QuestionStack questionStack;
   Topic(
       {Key? key,
       required this.answerType,
       required this.comingFrom,
-      required this.title})
+      required this.questionStack})
       : super(key: key);
 
   @override
@@ -41,12 +42,12 @@ class _TopicState extends State<Topic> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
-              widget.title,
+              widget.questionStack.name,
               style: TextStyle(fontSize: 30),
             ),
             Column(
               children: [
-                if (!widget.answerType) Icon(Icons.abc),
+                if (!widget.answerType && widget.comingFrom) Icon(Icons.abc),
                 if (widget.answerType) Icon(Icons.check_box_outline_blank),
                 if (!widget.comingFrom)
                   IconButton(
