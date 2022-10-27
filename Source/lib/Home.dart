67e3dcc7080b3646c3156e4cc9ba50cb86abccb4
Johanneskuptz <johannes.kuptz@web.de>
2022-10-27 15:02:48 +0200

    Override Back Button in Home

--- Source/lib/Home.dart
+++ Source/lib/Home.dart
@@ -18,46 +18,49 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.add_circle_outline),
-              onPressed: addNewDeck,
-            )
-          ],
-          title: const Text(
-            "Home",
-            style: TextStyle(fontSize: 50),
-          )),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Expanded(
-              child: ListView.builder(
-                  itemCount: widget.datahelper.amountOfQuestionStacks(),
-                  itemBuilder: (BuildContext context, int index) {
-                    QuestionStack questionStack =
-                        widget.datahelper.getQuestionStack(index);
-                    return Topic(
-                      datahelper: widget.datahelper,
-                      questionStack: questionStack,
-                      index: index,
-                      removeStack: () {
-                        setState(() {
-                          widget.datahelper.removeQuestionStack(index);
-                        });
-                      },
-                    );
-                  }),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [],
-            ),
-          ],
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.add_circle_outline),
+                onPressed: addNewDeck,
+              )
+            ],
+            title: const Text(
+              "Home",
+              style: TextStyle(fontSize: 50),
+            )),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                child: ListView.builder(
+                    itemCount: widget.datahelper.amountOfQuestionStacks(),
+                    itemBuilder: (BuildContext context, int index) {
+                      QuestionStack questionStack =
+                          widget.datahelper.getQuestionStack(index);
+                      return Topic(
+                        datahelper: widget.datahelper,
+                        questionStack: questionStack,
+                        index: index,
+                        removeStack: () {
+                          setState(() {
+                            widget.datahelper.removeQuestionStack(index);
+                          });
+                        },
+                      );
+                    }),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [],
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -77,5 +80,28 @@ class _HomeState extends State<Home> {
         });
       }
     });
+  }
+
+  Future<bool> _onWillPop() async {
+    return (await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: new Text('Are you sure?'),
+            content: new Text('Do you want to exit an App'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                //<-- SEE HERE
+                child: new Text('No'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                // <-- SEE HERE
+                child: new Text('Yes'),
+              ),
+            ],
+          ),
+        )) ??
+        false;
   }
 }
