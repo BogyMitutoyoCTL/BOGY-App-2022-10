a8b1c805fea59fa89470b8210be34d6f3b94c88b
Dieter Blocher <dieter.blocher@mitutoyo-ctl.de>
2022-10-27 11:28:33 +0200

    Change background color of questions in EditDeck...

--- Source/lib/TopicQuestion.dart
+++ Source/lib/TopicQuestion.dart
@@ -39,25 +39,28 @@ class _TopicQuestionState extends State<TopicQuestion> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ElevatedButton(
-          onPressed: editQuestion,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Flexible(
-                child: Text(
-                  questionName(),
-                  overflow: TextOverflow.visible,
-                  style: TextStyle(fontSize: 17),
+    return Container(
+      color: Colors.grey,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(
+            onPressed: editQuestion,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  child: Text(
+                    questionName(),
+                    overflow: TextOverflow.visible,
+                    style: TextStyle(fontSize: 17),
+                  ),
                 ),
-              ),
-              Icon(widget.isMultipleChoice
-                  ? Icons.check_box_outline_blank
-                  : Icons.abc),
-            ],
-          )),
+                Icon(widget.isMultipleChoice
+                    ? Icons.check_box_outline_blank
+                    : Icons.abc),
+              ],
+            )),
+      ),
     );
   }
 
