2e6b1fa28488ca5b01820af9a8ff16500412a777
Dieter Blocher <dieter.blocher@mitutoyo-ctl.de>
2022-10-27 13:41:21 +0200

    Try to show image...

--- Source/lib/EditQuestion.dart
+++ Source/lib/EditQuestion.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_final_fields, prefer_const_constructors, unused_local_variable
 
+import 'dart:io';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:learnhub/DataHelper/QuestionBasic.dart';
@@ -19,6 +21,7 @@ class EditQuestion extends StatefulWidget {
 class _EditQuestionState extends State<EditQuestion> {
   bool _isPictureQuestion = false;
   bool _isMultipleChoice = false;
+  String? _image_base64;
   final TextEditingController _titleController = TextEditingController();
   List<TextEditingController> answerControllers = [
     TextEditingController(),
@@ -29,50 +32,53 @@ class _EditQuestionState extends State<EditQuestion> {
   List<String> _answer = ["", "", "", "", ""];
   final ImagePicker _picker = ImagePicker();
 
-   String getFragenTitel(){
-    if(_isPictureQuestion){
+  String getFragenTitel() {
+    if (_isPictureQuestion) {
       return "";
-    }
-    else{
-      if(_isMultipleChoice){
-        QuestionStringAndAnswers textMultipleFrage = widget.frage as QuestionStringAndAnswers;
+    } else {
+      if (_isMultipleChoice) {
+        QuestionStringAndAnswers textMultipleFrage =
+            widget.frage as QuestionStringAndAnswers;
         return textMultipleFrage.question;
-      } else{
-        QuestionStringAndFreeText textBenutzerFrage = widget.frage as QuestionStringAndFreeText;
+      } else {
+        QuestionStringAndFreeText textBenutzerFrage =
+            widget.frage as QuestionStringAndFreeText;
         return textBenutzerFrage.question;
       }
     }
   }
 
-  void setRadioButtons(){
-     if(widget.frage.questionType == QuestionTypes.stringAndFreeText){
-       _isMultipleChoice = false;
-       _isPictureQuestion = false;
-     } else if(widget.frage.questionType == QuestionTypes.stringAndAnswers){
-       _isMultipleChoice = true;
-       _isPictureQuestion = false;
-     }
+  void setRadioButtons() {
+    if (widget.frage.questionType == QuestionTypes.stringAndFreeText) {
+      _isMultipleChoice = false;
+      _isPictureQuestion = false;
+    } else if (widget.frage.questionType == QuestionTypes.stringAndAnswers) {
+      _isMultipleChoice = true;
+      _isPictureQuestion = false;
+    }
   }
 
   @override
   void initState() {
     super.initState();
-     setRadioButtons();
-
-
-    _titleController.text =  getFragenTitel();
+    setRadioButtons();
+    print(_image_base64);
+    _titleController.text = getFragenTitel();
     setAntwort();
   }
-   void setAntwort(){
-    if(widget.frage.questionType == QuestionTypes.stringAndFreeText){
+
+  void setAntwort() {
+    if (widget.frage.questionType == QuestionTypes.stringAndFreeText) {
       _isMultipleChoice = false;
       _isPictureQuestion = false;
-      QuestionStringAndFreeText textBenutzerFrage = widget.frage as QuestionStringAndFreeText;
+      QuestionStringAndFreeText textBenutzerFrage =
+          widget.frage as QuestionStringAndFreeText;
       answerControllers[0].text = textBenutzerFrage.answer;
-    } else if(widget.frage.questionType == QuestionTypes.stringAndAnswers){
+    } else if (widget.frage.questionType == QuestionTypes.stringAndAnswers) {
       _isMultipleChoice = true;
       _isPictureQuestion = false;
-      QuestionStringAndAnswers textBenutzerFrage = widget.frage as QuestionStringAndAnswers;
+      QuestionStringAndAnswers textBenutzerFrage =
+          widget.frage as QuestionStringAndAnswers;
       answerControllers[0].text = textBenutzerFrage.answers[0];
       answerControllers[1].text = textBenutzerFrage.answers[1];
       answerControllers[2].text = textBenutzerFrage.answers[2];
@@ -92,78 +98,82 @@ class _EditQuestionState extends State<EditQuestion> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Karte bearbeiten"),
+      appBar: AppBar(
+        title: Text("Karte bearbeiten"),
       ),
       body: ListView(
         children: [
           Padding(
             padding: const EdgeInsets.all(10),
             child: Column(
-              children: [
-                    RadioListTile(
-                      value: false,
-                      groupValue: _isPictureQuestion,
-                      onChanged: questionTypeChange,
-                      title: Text("Frage"),
-                    ),
-                    RadioListTile(
-                      value: true,
-                      groupValue: _isPictureQuestion,
-                      onChanged: questionTypeChange,
-                      title: const Text("Bild"),
-                    ),
-                    if (!_isPictureQuestion)
+                children: [
+                      RadioListTile(
+                        value: false,
+                        groupValue: _isPictureQuestion,
+                        onChanged: questionTypeChange,
+                        title: Text("Frage"),
+                      ),
+                      RadioListTile(
+                        value: true,
+                        groupValue: _isPictureQuestion,
+                        onChanged: questionTypeChange,
+                        title: const Text("Bild"),
+                      ),
+                      if (!_isPictureQuestion)
+                        TextField(
+                          controller: _titleController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Frage",
+                          ),
+                        ),
+                      if (_isPictureQuestion) const Text("Bild:"),
+                      if (_isPictureQuestion)
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            if (_image_base64 != null)
+                              Image(
+                                  image:
+                                      Image.memory(base64Decode(_image_base64!)).image),
+                            IconButton(
+                              onPressed: importPic,
+                              icon: const Icon(Icons.image),
+                            ),
+                            IconButton(
+                              onPressed: importPic,
+                              icon: const Icon(Icons.camera_alt),
+                            ),
+                          ],
+                        ),
+                      RadioListTile(
+                        value: false,
+                        groupValue: _isMultipleChoice,
+                        onChanged: answerTypeChange,
+                        title: Text("Benutzereingabe"),
+                      ),
+                      RadioListTile(
+                        value: true,
+                        groupValue: _isMultipleChoice,
+                        onChanged: answerTypeChange,
+                        title: const Text("Multiple Choice"),
+                      ),
                       TextField(
-                        controller: _titleController,
+                        controller: answerControllers[0],
                         decoration: const InputDecoration(
                           border: OutlineInputBorder(),
-                          labelText: "Frage",
+                          labelText: "Richtige Antwort",
                         ),
-                      ),
-                    if (_isPictureQuestion) const Text("Bild:"),
-                    if (_isPictureQuestion)
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          IconButton(
-                            onPressed: importPic,
-                            icon: const Icon(Icons.image),
-                          ),
-                          IconButton(
-                            onPressed: importPic,
-                            icon: const Icon(Icons.camera_alt),
-                          ),
-                        ],
-                      ),
-                    RadioListTile(
-                      value: false,
-                      groupValue: _isMultipleChoice,
-                      onChanged: answerTypeChange,
-                      title: Text("Benutzereingabe"),
-                    ),
-                    RadioListTile(
-                      value: true,
-                      groupValue: _isMultipleChoice,
-                      onChanged: answerTypeChange,
-                      title: const Text("Multiple Choice"),
-                    ),
-                    TextField(
-                      controller: answerControllers[0],
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Richtige Antwort",
-                      ),
-                    )
-                  ] +
-                  multipleChoiceWidgets()
-            ),
+                      )
+                    ] +
+                    multipleChoiceWidgets()),
           ),
         ],
       ),
-floatingActionButton: FloatingActionButton(
-    onPressed: save,
-    child: const Icon(Icons.check),
-),
+      floatingActionButton: FloatingActionButton(
+        onPressed: save,
+        child: const Icon(Icons.check),
+      ),
     );
   }
 
@@ -185,8 +195,15 @@ floatingActionButton: FloatingActionButton(
   }
 
   Future<void> importPic() async {
-    //Funkt nur unter Android
-    XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    XFile? pickedImage = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      var imagepath = pickedImage.path;
+      File imagefile = File(imagepath); //convert Path to File
+      var imagebytes = await imagefile.readAsBytes(); //convert to bytes
+      String base64string = base64.encode(imagebytes); //convert bytes to base64 string
+      _image_base64 = base64string;
+      setState(() {});
+    }
   }
 
   void questionTypeChange(bool? type) {
@@ -221,15 +238,12 @@ floatingActionButton: FloatingActionButton(
             antwortnamefalsch3
           ],
         );
-        Navigator.of(context).pop(
-          frageMultiple
-        );
+        Navigator.of(context).pop(frageMultiple);
       } else {
         String antwortnamerichtig = answerControllers[0].text;
-        QuestionStringAndFreeText frageEingabe = QuestionStringAndFreeText(question: fragenname, answer: antwortnamerichtig);
-        Navigator.of(context).pop(
-          frageEingabe
-        );
+        QuestionStringAndFreeText frageEingabe = QuestionStringAndFreeText(
+            question: fragenname, answer: antwortnamerichtig);
+        Navigator.of(context).pop(frageEingabe);
       }
     }
   }
